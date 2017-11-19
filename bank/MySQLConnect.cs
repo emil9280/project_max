fe486cf9f15f9993baa3db5bbedce8f63753a526
emil9280 <33316283+emil9280@users.noreply.github.com>
2017-11-19 19:00:02 +0100

    Initial commit

---
+++ bank/MySQLConnect.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Threading;
+
+namespace bank
+{
+    class MySQLConnect
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        public MySQLConnect()
+        {
+            Initiallize();
+        }
+
+        private void Initiallize()
+        {
+            server = "localhost";
+            database = "users";
+            uid = "root";
+            password = "3milHM9685";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+            connection = new MySqlConnection(connectionString);
+        }
+
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch(MySqlException ex)
+            {
+                switch(ex.Number)
+                {
+                    case 0:
+                        Console.WriteLine("cannot connect to server");
+                        Thread.Sleep(5000);
+                        break;
+
+                    case 1045:
+                        Console.WriteLine("invalid username/password for the database");
+                        Thread.Sleep(5000);
+                        break;
+                }
+                return false;
+            }
+        }
+
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch(MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Thread.Sleep(5000);
+                return false;
+            }
+        }
+
+        public List<string>[] SelectLogin()
+        {
+            string query = "SELECT * FROM bankusers";
+
+            List<string>[] list = new List<string>[2];
+            list[0] = new List<string>();
+            list[1] = new List<string>();
+
+            if (this.OpenConnection())
+            {
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    list[0].Add(dataReader["username"] + "");
+                    list[1].Add(dataReader["password"] + "");
+                }
+
+                dataReader.Close();
+                this.CloseConnection();
+                return list;
+            }
+            else return list;
+        }
+    }
+}
---
+++ bank/Program.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Threading;
+
+namespace bank
+{
+    class Program
+    {
+        public void Screen1()
+        {
+            int screenwidth = Console.WindowWidth;
+
+            for (int i = 0; i < screenwidth; i++)
+            {
+                Console.SetCursorPosition(i, 0);
+                Console.WriteLine("#");
+            }
+        }
+        public void Screen2()
+        {
+            int screenheight = Console.WindowHeight;
+            int screenwidth = Console.WindowWidth;
+
+            for (int i = 0; i < screenwidth; i++)
+            {
+                Console.SetCursorPosition(i, screenheight - 1);
+                Console.Write("#");
+            }
+            for (int i = 0; i < screenheight - 1; i++)
+            {
+                Console.SetCursorPosition(0, i);
+                Console.Write("#");
+            }
+            for (int i = 0; i < screenheight - 1; i++)
+            {
+                Console.SetCursorPosition(screenwidth - 1, i);
+                Console.Write("#");
+            }
+        }
+        public void Loading()
+        {
+            Console.WindowHeight = 3;
+            Console.WindowWidth = 20;
+            int screenHeight = Console.WindowHeight;
+            int screenWidth = Console.WindowWidth;
+            string[] loadingImg = { "|", "/", "-", "\\", "|", "/", "-", "\\", "|", "/", "-", "\\", "|", "/", "-", "\\", "|", "/", "-", "\\", "|", "/", "-", "\\" };
+            string[] loadingDots = { ".", "..", "...", "....", ".....", "......", ".......", "", "" };
+            int loadingImgPos = 0;
+            int loadingDotsPos = 0;
+
+            try
+            {
+                for (int i = 0; i < 30; i++)
+                {
+                    Console.WriteLine(loadingImg[loadingImgPos] + " Loading " + loadingDots[loadingDotsPos]);
+                    System.Threading.Thread.Sleep(150);
+                    Console.Clear();
+
+                    if (loadingImgPos > 23)
+                    {
+                        loadingDotsPos = 0;
+                    }
+                    else
+                    {
+                        loadingImgPos++;
+                    }
+
+                    if (loadingDotsPos > 6)
+                    {
+                        loadingDotsPos = 0;
+                    }
+                    else
+                    {
+                        loadingDotsPos++;
+                    }
+                }
+
+            }
+            catch { }
+
+            Console.WriteLine("Files okay");
+            System.Threading.Thread.Sleep(500);
+            Console.Clear();
+            Login();
+        }
+
+
+
+
+        public void Login()
+        {
+            Console.WindowHeight = 15;
+            Console.WindowWidth = 40;
+            int screenHeight = Console.WindowHeight;
+            int screenWidth = Console.WindowWidth;
+            string Brugernavn;
+            string BRugernavn = "test";
+            string BrugerNavn;
+            string BRugerNavn = "emil9280";
+            string Password;
+            string PAssword = "1234";
+            string PAssWord = "hello";
+            int screenheight = Console.WindowHeight;
+            int screenwidth = Console.WindowWidth;
+
+
+
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Clear();
+            Screen1();
+            Console.WriteLine("#    Welcome to Bank Of TechCollege");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine("#      press any button to login       #");
+            Screen2();
+            Console.ReadKey();
+            Console.Clear();
+            Screen1();
+            Console.WriteLine("Please enter login data");
+            Thread.Sleep(300);
+            Console.Write("username: ");
+            Brugernavn = Console.ReadLine();
+            Console.Write("password: ");
+            Password = Console.ReadLine();
+            #region Lars
+            MySQLConnect mysql = new MySQLConnect();
+            List<string>[] list = mysql.SelectLogin();
+            if (UserCheck(list[0], list[1], Brugernavn, Password))
+            {
+                Console.WriteLine("You have logged in");
+                Thread.Sleep(10000);
+                //you have loged in
+            }
+            else
+            {
+                Console.WriteLine("Login or password was incorrect");
+                Thread.Sleep(5000);
+                //password or username was wrong
+            }
+            #endregion
+            /*
+            string MyconnectionString = "Server=localhost:3306;Database=users;uid=" + Brugernavn + ";pwd=" + Password + ";";
+            MySqlConnection connection = new MySqlConnection(MyconnectionString);
+            connection.Open();
+            Console.WriteLine("Hello " + Brugernavn);
+            Thread.Sleep(500);
+            */
+        }
+
+        #region Lars
+        public bool UserCheck(List<string> user, List<string> password, string userInput, string passInput)
+        {
+            bool returnvalue = false;
+            for(int i = 0; i < user.Count; i++)
+            {
+                if(user[i] == userInput)
+                {
+                    if(password[i] == passInput)
+                    {
+                        returnvalue = true;
+                    }
+                }
+            }
+
+            return returnvalue;
+        }
+        #endregion
+
+
+        public void Menu()
+        {
+            string resultat;
+            int resultat2;
+
+            try
+            {
+                Console.WriteLine("Please choose where to go to");
+                Console.WriteLine("");
+                Console.WriteLine("press 1 for account");
+                Console.WriteLine("press 2 for transfere");
+                resultat = Console.ReadLine();
+                resultat2 = Convert.ToInt16(resultat);
+                if (resultat2 == 1)
+                {
+                    Console.Clear();
+                    Account();
+                }
+                else
+                {
+                    if (resultat2 == 2)
+                    {
+                        Console.Clear();
+                        Transfere();
+                    }
+                }
+            }
+            catch
+            { }
+            Console.ReadKey();
+        }
+
+
+
+
+        public void Account()
+        {
+            Console.WriteLine("hello");
+            Console.ReadKey();
+        }
+
+
+
+
+        public void Transfere()
+        {
+            Console.WriteLine("hello2");
+            Console.ReadKey();
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            new Program().Loading();
+        }
+    }
+}
